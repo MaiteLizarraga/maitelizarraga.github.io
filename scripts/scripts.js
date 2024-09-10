1e42226befb1a0663af1e4eb350d96da3a4e151c
MaiteLizarraga <maite.lizarraga@gmail.com>
2024-09-10 11:46:38 +0200

    index modifs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,20 @@ window.addEventListener('load', function () {
     document.body.classList.add('loaded');
 });
 
-$('.grid').masonry({
-// options
-itemSelector: '.grid-item',
-columnWidth: 200
+// $('.grid').masonry({
+// // options
+// itemSelector: '.grid-item',
+// columnWidth: 200
+// });
+
+var grid = document.querySelector('.grid');
+var msnry;
+
+// Initialize Masonry after all images have loaded
+imagesLoaded(grid, function () {
+  msnry = new Masonry(grid, {
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: 80
+  });
 });
